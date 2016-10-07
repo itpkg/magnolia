0f92ad6222c22c5be64c1e517d96dde8b847ad42
Jitang Zheng <jitang.zheng@gmail.com>
2016-10-07 14:43:45 -0700

    aes and ssha

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	_ "github.com/itpkg/magnolia/routers"
 	"github.com/astaxie/beego"
+	_ "github.com/itpkg/magnolia/routers"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	beego.Run()
 }
-
---
+++ utils/encryptor_test.go
@@ -0,0 +1,46 @@
+package utils_test
+
+import (
+	"crypto/aes"
+	"testing"
+
+	"github.com/itpkg/magnolia/utils"
+)
+
+func TestTextEncryptor(t *testing.T) {
+	cip, err := aes.NewCipher([]byte("1234567890123456"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	enc := utils.TextEncryptor{Cipher: cip}
+	hello := "Hello, magnolia!"
+	code, err := enc.Encode([]byte(hello))
+	if err != nil {
+		t.Fatal(err)
+	}
+	plain, err := enc.Decode(code)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(plain) != hello {
+		t.Fatalf("wang %s get %s", hello, string(plain))
+	}
+}
+
+func TestPasswordEncryptor(t *testing.T) {
+	enc := utils.PasswordEncryptor{}
+	plain := "123456"
+	code, err := enc.Sum([]byte(plain), 8)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("doveadm pw -t {SSHA512}%s -p %s", code, plain)
+	rst, err := enc.Equal([]byte(plain), code)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !rst {
+		t.Fatalf("check password failed")
+	}
+}
